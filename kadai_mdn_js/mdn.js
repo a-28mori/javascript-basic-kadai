1ca9712b5866525925299511a2ea0f0739722ee0
a-28mori <mori28nov@icloud.com>
2026-04-04 20:51:54 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+const today = new Date(2024, 9, 12);
+
+const year = today.getFullYear();
+const month = today.getMonth() + 1;
+const day = today.getDate();
+
+console.log(`${year}年${month}月${day}日`);
